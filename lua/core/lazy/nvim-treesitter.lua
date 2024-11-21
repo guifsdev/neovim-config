8fc3b02bd7964b4e4dedadd658d917d6a9d5f751
Guilherme <guifsdev@gmail.com>
2024-11-20 23:00:26 -0300

    Added tree-sitter-blade to allow syntax highlight in laravel blade files

--- lua/core/lazy/nvim-treesitter.lua
+++ lua/core/lazy/nvim-treesitter.lua
@@ -27,7 +27,29 @@ return { -- Highlight, edit, and navigate code
 			additional_vim_regex_highlighting = { "ruby" },
 		},
 		indent = { enable = true, disable = { "ruby" } },
+
 	},
+
+	config = function (_, opts)
+		local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = {"src/parser.c"},
+				branch = "main",
+			},
+			filetype = "blade"
+		}
+
+		vim.filetype.add({
+			pattern = {
+				['.*%.blade%.php'] = 'blade',
+			},
+		})
+
+		require('nvim-treesitter.configs').setup(opts)
+	end,
 	-- There are additional nvim-treesitter modules that you can use to interact
 	-- with nvim-treesitter. You should go explore a few and see what interests you:
 	--
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -33,5 +33,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostic hover window' })
 
 vim.keymap.set("n", "-", vim.cmd.Ex)
